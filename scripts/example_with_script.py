05f5a2a6e0a60372bd0d3e8c9c7c84feb9e6f061
ctbus <ctbushman@gmail.com>
2022-12-13 14:39:05 -0500

    Add debug

--- scripts/example_with_script.py
+++ scripts/example_with_script.py
@@ -1,5 +1,7 @@
 import shutil
+import sys
 
+sys.stderr(f"Copying {snakemake.input} to {snakemake.output}...")
 shutil.copyfile(snakemake.input, snakemake.output)
 with open(snakemake.log, "w") as f:
     f.write("File copied!")

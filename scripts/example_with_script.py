9dcd42083dd05cde85a5b8dc8c1a14861de65ff5
ctbus <ctbushman@gmail.com>
2022-12-13 14:57:31 -0500

    Testing types

--- scripts/example_with_script.py
+++ scripts/example_with_script.py
@@ -1,6 +1,7 @@
 import sys
 
-#sys.stderr(f"Copying {snakemake.input} to {snakemake.output}...")
+sys.stderr(f"Copying {type(snakemake.input)} to {type(snakemake.output)}...")
+sys.stderr(f"{snakemake.input[0]}")
 snakemake.output.write(snakemake.input.readlines())
 with open(snakemake.log, "w") as f:
     f.write("File copied!")

0c712cdeb274b9fe8a11348955a2f05d11099c16
ctbus <ctbushman@gmail.com>
2022-12-13 15:04:57 -0500

    Trying with pathlib

--- scripts/example_with_script.py
+++ scripts/example_with_script.py
@@ -1,7 +1,5 @@
-import sys
+from pathlib import Path
 
-sys.stderr(f"Copying {type(snakemake.input)} to {type(snakemake.output)}...")
-sys.stderr(f"{snakemake.input[0]}")
-snakemake.output.write(snakemake.input.readlines())
+Path(snakemake.output[0]).symlink_to(snakemake.input[0])
 with open(snakemake.log, "w") as f:
     f.write("File copied!")

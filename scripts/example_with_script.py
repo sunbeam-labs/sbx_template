987c9d4be4d57caccc38446979fb47b1563e1636
ctbus <ctbushman@gmail.com>
2022-12-13 14:45:51 -0500

    Better file copy

--- scripts/example_with_script.py
+++ scripts/example_with_script.py
@@ -1,7 +1,6 @@
-import shutil
 import sys
 
-sys.stderr(f"Copying {snakemake.input} to {snakemake.output}...")
-shutil.copyfile(snakemake.input, snakemake.output)
+#sys.stderr(f"Copying {snakemake.input} to {snakemake.output}...")
+snakemake.output.write(snakemake.input.readlines())
 with open(snakemake.log, "w") as f:
     f.write("File copied!")

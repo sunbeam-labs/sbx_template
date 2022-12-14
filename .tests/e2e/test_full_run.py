e324e8858eacf8d1d018bd85bf0c422731dfea7e
ctbus <ctbushman@gmail.com>
2022-12-14 17:08:12 -0500

    Use fixture-based tests

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -1,58 +1,80 @@
+import csv
 import os
+import pytest
 import shutil
 import subprocess as sp
 import tempfile
-import unittest
 
 
-class FullRunTests(unittest.TestCase):
-    def setUp(self) -> None:
-        self.temp_dir = tempfile.mkdtemp()
+@pytest.fixture
+def setup():
+    temp_dir = tempfile.mkdtemp()
 
-        self.reads_fp = ".tests/data/reads/"
+    reads_fp = os.path.abspath(".tests/data/reads/")
 
-        self.project_dir = os.path.join(self.temp_dir, "project/")
+    project_dir = os.path.join(temp_dir, "project/")
 
-        sp.check_output(
-            ["sunbeam", "init", "--data_fp", self.reads_fp, self.project_dir]
-        )
+    sp.check_output(
+        ["sunbeam", "init", "--data_fp", reads_fp, project_dir]
+    )
 
-        self.config_fp = os.path.join(self.project_dir, "sunbeam_config.yml")
+    config_fp = os.path.join(project_dir, "sunbeam_config.yml")
 
-        config_str = f"sbx_template: {{example_rule_options: '--number'}}"
+    config_str = f"sbx_template: {{example_rule_options: '--number'}}"
 
-        sp.check_output(
-            [
-                "sunbeam",
-                "config",
-                "modify",
-                "-i",
-                "-s",
-                f"{config_str}",
-                f"{self.config_fp}",
-            ]
-        )
+    sp.check_output(
+        [
+            "sunbeam",
+            "config",
+            "modify",
+            "-i",
+            "-s",
+            f"{config_str}",
+            f"{config_fp}",
+        ]
+    )
 
-        self.output_fp = os.path.join(self.project_dir, "sunbeam_output")
+    yield project_dir, temp_dir
 
-        self.big_file_fp = os.path.join(self.output_fp, "qc/mush/big_file.txt")
+    shutil.rmtree(temp_dir)
 
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
+@pytest.fixture
+def run_sunbeam(setup):
+    temp_dir, project_dir = setup
 
-    def test_full_run(self):
-        # Run the test job.
-        sp.check_output(
-            [
-                "sunbeam",
-                "run",
-                "--profile",
-                self.project_dir,
-                "all_template",
-                "--directory",
-                self.temp_dir,
-            ]
-        )
+    # Run the test job.
+    sp.check_output(
+        [
+            "sunbeam",
+            "run",
+            "--profile",
+            project_dir,
+            "all_classify",
+            "--directory",
+            temp_dir,
+        ]
+    )
 
-        # Check output
-        self.assertTrue(os.path.exists(self.big_file_fp))
+    output_fp = os.path.join(project_dir, "sunbeam_output")
+
+    big_file_fp = os.path.join(output_fp, "qc/mush/big_file.txt")
+
+    benchmarks_fp = os.path.join(project_dir, "stats/")
+
+    yield big_file_fp, benchmarks_fp
+
+def test_full_run(run_sunbeam):
+    big_file_fp, benchmarks_fp = run_sunbeam
+
+    # Check output
+    assert os.path.exists(big_file_fp)
+
+def test_benchmarks(run_sunbeam):
+    big_file_fp, benchmarks_fp = run_sunbeam
+
+    filename = os.listdir(benchmarks_fp)[0]
+    with open(os.path.join(benchmarks_fp, filename)) as f:
+        rd = csv.DictReader(f, delimiter="\t")
+        for r in rd:
+            assert r == 0
+            # assert float(r["cpu_time"]) < 0.0

7e794966a01c9b9e22fd41ad6d572d9ae02aba92
ctbus <ctbushman@gmail.com>
2022-12-14 22:42:03 -0500

    Reformat

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -77,4 +77,6 @@ def test_benchmarks(run_sunbeam):
     with open(os.path.join(benchmarks_fp, filename)) as f:
         rd = csv.DictReader(f, delimiter="\t")
         for r in rd:
-            assert float(r["cpu_time"]) < 0.5
+            assert (
+                float(r["cpu_time"]) < 0.5
+            ), f"cpu_time for {r['rule']} is higher than 0.5: {r['cpu_time']}"
